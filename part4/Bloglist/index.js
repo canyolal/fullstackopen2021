ffe4f0c1b496212877741d3fdbc6da443630b832
canyolal <canyolal16@gmail.com>
2021-10-23 20:41:49 +0300

    part4a 4.1 and 4.2 finito

--- part4/Bloglist/index.js
+++ part4/Bloglist/index.js
@@ -1,25 +1,11 @@
 const http = require("http")
-const express = require("express")
-const app = express()
-const cors = require("cors")
-const mongoose = require('mongoose')
 const config = require("./utils/config")
 const logger = require("./utils/logger")
-const blogsRouter = require("./controllers/blogs")
+const app = require("./app")
 
+const server = http.createServer(app)
 
-mongoose.connect(config.MONGODB_URI)
-  .then(() => {
-    logger.log("conntected to MONGODB server")
-  })
-  .catch(error => logger.error("CANNOT connect to MongoDB", error.message))
-
-app.use(cors())
-app.use(express.json())
-
-app.use("/api/blogs", blogsRouter)
-
-http.createServer()
+server.listen()
 app.listen(config.PORT, () => {
   logger.log(`Server running on port ${config.PORT}`)
 })
